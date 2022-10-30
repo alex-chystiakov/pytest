01daf8b3fa364ec604f975468c6f60a9162d1c37
alex-chystiakov <aleksandr.chystiakov@gmail.com>
2022-10-30 18:41:00 +0200

    wert1

--- app.py
+++
@@ -1,8 +0,0 @@
-def find_min_max(data):
-    min_num = min(data)
-    max_num = max(data)
-    return (min_num, max_num)
-
-
-minimum, maximum = find_min_max([1.3, 54, 0.4, 100, 456])
-print(minimum, maximum)
