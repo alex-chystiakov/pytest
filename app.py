d766a68fc3e6b5a6e2cd377eaabc596de2b489cd
alex-chystiakov <aleksandr.chystiakov@gmail.com>
2022-10-30 17:45:51 +0200

    'Unpacking'

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('Hello world!')
+def find_min_max(data):
+    min_num = min(data)
+    max_num = max(data)
+    return (min_num, max_num)
+
+
+minimum, maximum = find_min_max([1.3, 54, 0.4, 100, 456])
+print(minimum, maximum)
